3966e3bb32ad2dcbcb95788aa427ab7b2462bae7
Michi <71011437+Gen67@users.noreply.github.com>
2026-03-29 16:21:26 +0800

    Add files via upload

---
+++ Date/code.js
@@ -0,0 +1,18 @@
+function clock(){
+    const now = new Date();
+    let hour = now.getHours();
+    const meridiem = hour >=12  ? "PM" : "AM";
+    hour = hour % 12 || 12; 
+    hour = hour.toString().padStart(2,0);
+
+
+    const minute = now.getMinutes().toString().padStart(2,0);
+    const second = now.getSeconds().toString().padStart(2,0);
+    const time = `${hour}:${minute}:${second} ${meridiem}`;
+    document.getElementById("time-day").textContent = time;
+
+}
+
+
+clock();
+setInterval(clock, 1000);
